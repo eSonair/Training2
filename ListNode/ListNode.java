f71bdca8dad054389e25e417705317319558b384
Heini Luo <heiniluo@msn.com>
2023-07-12 12:07:44 +0200

    Update

---
+++ ListNode/ListNode.java
@@ -0,0 +1,19 @@
+package Training2.ListNode;
+
+public class ListNode <T> {
+  T val;
+  ListNode next;
+
+  ListNode() {
+
+  }
+
+  ListNode(T val) {
+   this.val = val;
+  }
+
+  ListNode(T val, ListNode next) {
+   this.val = val;
+   this.next = next;
+  }
+}
